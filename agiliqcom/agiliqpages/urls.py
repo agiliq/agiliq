ae5f07ad148d4180f84c9eb9be5f7520fa17179d
Shabda Raaj <shabda@uswaretech.com>
2010-05-25 18:59:40 +1000

    Change sto urls

--- agiliqcom/agiliqpages/urls.py
+++ agiliqcom/agiliqpages/urls.py
@@ -5,9 +5,9 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns('agiliqpages.views',
 
     url('^$', direct_to_template, {"template": "agiliqpages/index.html"}, name='agiliqpages_index'),
-    url('^contactus$', direct_to_template, {"template_name": "agiliqpages/contactus.html"}, name='agiliqpages_index'),
-    url('^ourwork$', direct_to_template, {"template_name": "agiliqpages/ourwork.html"}, name='agiliqpages_index'),
-    url('^whatwedo$', direct_to_template, {"template_name": "agiliqpages/whatwedo.html"}, name='agiliqpages_index'),
-    url('^whoweare$', direct_to_template, {"template_name": "agiliqpages/whoweare.html"}, name='agiliqpages_index'),
+    url('^contactus$', direct_to_template, {"template": "agiliqpages/contactus.html"}, name='agiliqpages_contactus'),
+    url('^ourwork$', direct_to_template, {"template": "agiliqpages/ourwork.html"}, name='agiliqpages_ourwork'),
+    url('^whatwedo$', direct_to_template, {"template": "agiliqpages/whatwedo.html"}, name='agiliqpages_whatwedo'),
+    url('^whoweare$', direct_to_template, {"template": "agiliqpages/whoweare.html"}, name='agiliqpages_whoweare'),
     
     )
--- agiliqcom/urls.py
+++ agiliqcom/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,3 +18,9 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.views.static',
+        (r'^site_media/(?P<path>.*)$', 'serve', { 'document_root': settings.MEDIA_ROOT,
+                                        'show_indexes': True }),
+    )

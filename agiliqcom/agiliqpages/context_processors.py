e2e3b5fdaec7570c128c8b92e91a78cc9feb924e
Shabda <shabda@Shabdas-iMac.local>
2010-05-31 20:34:38 +0530

    testimonial, clients, products values are now taken from DB

--- agiliqcom/agiliqpages/context_processors.py
+++ agiliqcom/agiliqpages/context_processors.py
@@ -1,5 +1,7 @@
 
-from agiliqpages.models import ContentBlock
+from agiliqpages.models import ContentBlock, Client
 
 def sidebar_vars(request):
-	return {'hire_us': ContentBlock.objects.get(slug='hire-us').content}
+	testimonials = Client.objects.filter(has_testimonial=True).order_by('?')
+	return {'hire_us': ContentBlock.objects.get(slug='hire-us').content,
+	        'testimonials': testimonials}
--- agiliqcom/agiliqpages/views.py
+++ agiliqcom/agiliqpages/views.py
@@ -6,6 +6,7 @@ from django.template import RequestContext
 from django.template.loader import render_to_string
 
 from agiliqpages.forms import ContactUsForm
+from agiliqpages.models import Client, Project
 
 def contact_us(request, template):
 	if request.method == 'POST':
@@ -20,4 +21,17 @@ def contact_us(request, template):
 			return redirect(reverse('agiliqpages_contactus'))
 	else:
 		form = ContactUsForm()
-	return render_to_response(template, {'form': form, 'sitepage': 'contactus'}, RequestContext(request))
+	return render_to_response(template, 
+							  {'form': form, 
+							   'sitepage': 'contactus'}, 
+							  RequestContext(request))
+
+
+def our_work(request, template):
+	products = Project.objects.all()
+	clients = Client.objects.all()
+	return render_to_response(template, 
+	                          {'products': products, 
+	                           'clients': clients, 
+	                           'sitepage': 'ourwork'}, 
+	                          RequestContext(request))

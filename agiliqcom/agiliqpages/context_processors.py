10376cbfebf630699b75a1b40e7dba4c280c32e8
ashok <ashok@ashok-laptop.(none)>
2010-06-02 00:20:05 +0530

    our code block

--- agiliqcom/agiliqpages/context_processors.py
+++ agiliqcom/agiliqpages/context_processors.py
@@ -1,18 +1,25 @@
 
 from agiliqpages.models import ContentBlock, Client, Tweet
 
+def get_object_or_none(model, **kwargs):
+    try:
+        return model.objects.get(**kwargs)
+    except model.DoesNotExist:
+        return None
+    
 def sidebar_vars(request):
     testimonials = Client.objects.filter(has_testimonial=True).order_by('?')
-    try:
-        hire_us = ContentBlock.objects.get(slug='hire-us').content
-    except:
-        hire_us = None
+    hire_us = get_object_or_none(ContentBlock, slug='hire-us')
+    hire_us = hire_us and hire_us.content
+    our_code = get_object_or_none(ContentBlock, slug='our-code')
+    our_code = our_code and our_code.content
 		
     try:
         tweet = Tweet.objects.latest()
     except:
         tweet = None
         
-    return {'hire_us': hire_us, 
+    return {'hire_us': hire_us,
+            'our_code': our_code, 
 		    'testimonials': testimonials,
 		    'tweet': tweet}

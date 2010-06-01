4053eea691c9458db287417bae41dfe0194b747d
Shabda Raaj <shabda@uswaretech.com>
2010-06-01 19:36:03 +1000

    Cahe generic views

--- agiliqcom/agiliqpages/urls.py
+++ agiliqcom/agiliqpages/urls.py
@@ -1,16 +1,30 @@
 from django.conf.urls.defaults import *
 
 from agiliqpages.models import TeamMember
+from django.views.generic.simple import direct_to_template
+from django.views.generic.list_detail import object_list
 
-urlpatterns = patterns('django.views.generic.simple',
+from django.views.decorators.cache import cache_page
+ONE_DAY = 60 * 60 * 24
+
+
+
+def cache(page):
+    return cache_page(page, ONE_DAY)
+
+cached_direct_to_template = cache(direct_to_template)
+cached_object_list = cache(object_list)
+
+
+urlpatterns = patterns('',
     url('^$', 
-		'direct_to_template', 
+		cached_direct_to_template, 
 		{'template': 'agiliqpages/index.html',
 		 'extra_context': {'sitepage': 'home'},
 		}, 
 		name='agiliqpages_index'),
     url('^whatwedo$', 
-		'direct_to_template', 
+		cached_direct_to_template, 
 		{'template': 'agiliqpages/whatwedo.html', 
 		 'extra_context': {'sitepage': 'whatwedo'},
 		}, 
@@ -20,9 +34,9 @@ urlpatterns = patterns('django.views.generic.simple',
 # using object_list will not cache the queryset
 # if team list is passed to direct_to_template as
 # a variable then the queryset will be cached
-urlpatterns += patterns('django.views.generic.list_detail',
+urlpatterns += patterns('',
 	url('^whoweare$', 
-	 	'object_list', 
+	 	cached_object_list, 
 	 	{'queryset': TeamMember.objects.all().order_by('ordering'),
 	     'template_name': 'agiliqpages/whoweare.html',
 	 	 'extra_context': {'sitepage': 'whoweare'}

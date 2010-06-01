170ab62704c9192071b86d5b1dbbb836d6056c0a
Shabda Raaj <shabda@uswaretech.com>
2010-06-01 18:11:33 +1000

    Dont erro when content block doesnt exist.

--- agiliqcom/agiliqpages/context_processors.py
+++ agiliqcom/agiliqpages/context_processors.py
@@ -3,5 +3,9 @@ from agiliqpages.models import ContentBlock, Client
 
 def sidebar_vars(request):
 	testimonials = Client.objects.filter(has_testimonial=True).order_by('?')
-	return {'hire_us': ContentBlock.objects.get(slug='hire-us').content,
+	try:
+		hire_us = ContentBlock.objects.get(slug='hire-us').content
+	except:
+		hire_us = None
+	return {'hire_us': hire_us,
 	        'testimonials': testimonials}

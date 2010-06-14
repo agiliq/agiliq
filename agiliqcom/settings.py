62a8cc2304a77cecdbf8f9e08c6a24879db619df
ashok <ashok@ashok-laptop.(none)>
2010-06-14 11:18:05 +0530

    fix #34

--- agiliqcom/settings.py
+++ agiliqcom/settings.py
@@ -66,7 +66,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 	'django.core.context_processors.i18n',
 	'django.core.context_processors.media',
 	'django.contrib.messages.context_processors.messages',
-	'agiliqpages.context_processors.sidebar_vars',
+	'agiliqpages.context_processors.extra_context',
     'blogango.context_processors.sidebar_vars',
 )
 

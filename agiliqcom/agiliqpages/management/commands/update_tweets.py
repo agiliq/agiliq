e19d8b2edcd0e91477752a0b3fb165d8b0002fd2
ashok.raavi <ashok.raavi@gmail.com>
2010-09-05 10:00:43 +0530

    using tweepy(oauth implementation) for updating tweets

--- agiliqcom/agiliqpages/management/commands/update_tweets.py
+++ agiliqcom/agiliqpages/management/commands/update_tweets.py
@@ -1,4 +1,7 @@
 
+import sys
+import tweepy
+
 from django.conf import settings
 from django.core.management.base import NoArgsCommand
 
@@ -9,17 +12,20 @@ class Command(NoArgsCommand):
     help = 'Update the tweets of user'
 
     def handle_noargs(self, **options):
-        twitter = Twitter(email=settings.TWITTER_API_USER, password=settings.TWITTER_API_PASSW)
+        auth = tweepy.OAuthHandler(settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_SECRET)
+        auth.set_access_token(settings.TWITTER_ACCESS_KEY, settings.TWITTER_ACCESS_SECRET)
+        
+        api = tweepy.API(auth)
         
         for scr_name in settings.TWITTER_FOLLOW:
-            user_tweets = twitter.statuses.user_timeline(screen_name=scr_name)
-            for tweet in user_tweets[::-1]:
-                tweet_id = tweet['id']
+            user_tweets = api.user_timeline(screen_name=scr_name)
+            for tweet in user_tweets:
+                tweet_id = tweet.id
                 tweet_exists = Tweet.objects.filter(tweet_id=tweet_id).count()
                 if tweet_exists:
                     continue
                 else:
-                    Tweet.objects.create(screen_name = tweet['user']['screen_name'], 
-                                         text=tweet['text'],
+                    Tweet.objects.create(screen_name = tweet.user.screen_name, 
+                                         text=tweet.text,
                                          tweet_id=tweet_id)
 

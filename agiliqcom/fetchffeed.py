29d1e7f88cb3556bcbe950673cbb4c0d1aa2f8bf
ashok <ashok@agiliq.com>
2010-10-11 12:54:52 +0000

    dont symlink

--- agiliqcom/fetchffeed.py
+++ agiliqcom/fetchffeed.py
@@ -1,1 +1,12 @@
-django-buzz_repo/fetchffeed.py
+
+from django.core.management import setup_environ
+import settings
+setup_environ(settings) 
+
+
+from pystories.services.EntryService import FetchManager
+
+
+fmanager = FetchManager()
+fmanager.triggerfetchFeed()
+

89e29bd2989d3517676afaf056256429003bd562
Venkata Ramana <ramana@agiliq.com>
2012-11-16 19:43:12 +0530

    1. Add list display for contactus ticket, 2. Add inlines for client. refer tickets 143, 144 in agiliq-misc

--- agiliqcom/agiliqpages/admin.py
+++ agiliqcom/agiliqpages/admin.py
@@ -3,16 +3,42 @@ from django.contrib import admin
 
 from agiliqpages.models import *
 
+
 class ContentBlockAdmin(admin.ModelAdmin):
 	prepopulated_fields = {'slug': ('name',)}
+
+
+class ContactUsListAdmin(admin.ModelAdmin):   
+        list_display = ('name', 'phone', 'email', 'company', 'created_at')
+        class Meta:
+            model = ContactUs
+
+
+class TestimonialInline(admin.TabularInline):
+	model = Testimonial
+        extra = 1
+
+class ContactAdmin(admin.ModelAdmin):
+	model = Contact
+        inlines = [ TestimonialInline ]
+
+
+class ContactInline(admin.TabularInline):
+        model = Contact
+	extra = 1
+
+class ClientAdmin(admin.ModelAdmin):        
+	inlines = [ ContactInline ]
 
 admin.site.register(ContentBlock, ContentBlockAdmin)
 
 admin.site.register(BlogEntry)
-admin.site.register(Client)
-admin.site.register(Contact)
+#admin.site.register(Client)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Contact, ContactAdmin)
 admin.site.register(Testimonial)
-admin.site.register(ContactUs)
+#admin.site.register(ContactUs)
+admin.site.register(ContactUs, ContactUsListAdmin)
 admin.site.register(Project)
 admin.site.register(TeamMember)
 admin.site.register(Whitepaper)

150d03075009fa0191724f0ec3e8967f52e453ae
Shabda Raaj <shabda@agiliq.com>
2012-12-03 16:36:41 +0530

    Added a dinette setting

--- agiliqcom/localsettings.example.py
+++ agiliqcom/localsettings.example.py
@@ -11,13 +11,8 @@ DATABASES = {
 }
 
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
 
-
+REPLY_PAGE_SIZE = 10
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 

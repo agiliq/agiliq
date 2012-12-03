d67bf9b4f9154906fd58d276e3937cfc8babf19b
Shabda Raaj <shabda@agiliq.com>
2012-12-03 13:29:09 +0530

    Added

--- agiliqcom/media/blogango/assets/js/superfish-1.4.8/superfish.function.js
+++
@@ -1,7 +0,0 @@
-		// initialise plugins
-        $(document).ready(function() { 
-            $('ul.sf-menu').superfish({
-                autoArrows:  true,                           // disable generation of arrow mark-up 
-                dropShadows: false                            // disable drop shadows 
-            }); 
-        });

358d706e42ddc52bbb11d1c5d8c0828ceb381b56
Shabda Raaj <shabda@agiliq.com>
2012-12-03 17:08:55 +0530

    Removed socialauth settings

--- agiliqcom/localsettings.example.py
+++ agiliqcom/localsettings.example.py
@@ -16,53 +16,6 @@ REPLY_PAGE_SIZE = 10
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-# Django-Socialauth settings
-LOGIN_REDIRECT_URL = '/forum/'
-LOGOUT_REDIRECT_URL = '/'
-LOGIN_URL = '/socialauth/login/'
-
-OPENID_REDIRECT_NEXT = '/socialauth/openid/done/'
-
-OPENID_SREG = {"requred": "nickname, email, fullname",
-               "optional":"postcode, country",
-               "policy_url": ""}
-
-#example should be something more like the real thing, i think
-OPENID_AX = [{"type_uri": "http://axschema.org/contact/email",
-              "count": 1,
-              "required": True,
-              "alias": "email"},
-             {"type_uri": "http://axschema.org/schema/fullname",
-              "count":1 ,
-              "required": False,
-              "alias": "fname"}]
-
-OPENID_AX_PROVIDER_MAP = {'Google': {'email': 'http://axschema.org/contact/email',
-                                     'firstname': 'http://axschema.org/namePerson/first',
-                                     'lastname': 'http://axschema.org/namePerson/last'},
-                          'Default': {'email': 'http://axschema.org/contact/email',
-                                      'fullname': 'http://axschema.org/namePerson',
-                                      'nickname': 'http://axschema.org/namePerson/friendly'}
-                          }
-
-TWITTER_CONSUMER_KEY = ''
-TWITTER_CONSUMER_SECRET = ''
-
-FACEBOOK_APP_ID = ''
-FACEBOOK_API_KEY = ''
-FACEBOOK_SECRET_KEY = ''
-
-LINKEDIN_CONSUMER_KEY = ''
-LINKEDIN_CONSUMER_SECRET = ''
-
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'socialauth.auth_backends.OpenIdBackend',
-    'socialauth.auth_backends.TwitterBackend',
-    'socialauth.auth_backends.FacebookBackend',
-    'socialauth.auth_backends.LinkedInBackend',
-)
-
 BACKTYPE_API_KEY = ''
 
 import markdown
@@ -86,6 +39,3 @@ DEFAULT_MARKUP_TYPES.append(('bbcode', render_bbcode))
 MARKUP_RENDERERS = DEFAULT_MARKUP_TYPES
 
 DEFAULT_MARKUP_TYPE = "markdown"
-
-TWITTER_API_USER = 'agiliqtest'
-TWITTER_API_PASSW = ''

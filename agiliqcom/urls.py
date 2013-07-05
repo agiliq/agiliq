07e202a1c5cc7b81ff336fabc16d7844ec606fca
Javed Khan <javed@agiliq.com>
2013-07-05 15:51:31 +0530

    fixed dinette login

--- agiliqcom/urls.py
+++ agiliqcom/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 from django.contrib import admin
+from django.contrib.auth.views import login
 from django.views.generic.base import TemplateView
+
+from socialauth.urls import urlpatterns as socialauth_urlpatterns
 
 admin.autodiscover()
 
@@ -13,6 +16,7 @@ urlpatterns = patterns('',
 	url(r'^forum/', include('dinette.urls')),
 	url(r'^blog/', include('blogango.urls')),
     url(r'^pystories/', include('pystories.urls')),
+    url(r'^login/$', login, name='dinette_login'),
 
     url(r'^admin/', include(admin.site.urls)),
     url('^404$', TemplateView.as_view(template_name="404.html"), name='agiliqcom_notfound'),
@@ -29,3 +33,5 @@ if settings.DEBUG or getattr(settings, 'SERVE_MEDIA', False):
     urlpatterns += patterns('django.contrib.staticfiles.views',
         url(r'^static/(?P<path>.*)$', 'serve'),
     )
+
+urlpatterns += socialauth_urlpatterns

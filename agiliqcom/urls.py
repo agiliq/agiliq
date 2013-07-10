44b1983480ee1bb38917554e278921a5eb4b740a
Javed Khan <javed@agiliq.com>
2013-07-10 13:30:26 +0530

    move dinette to /demo

--- agiliqcom/urls.py
+++ agiliqcom/urls.py
@@ -13,7 +13,7 @@ handler500 = 'agiliqpages.views.server_error'
 
 urlpatterns = patterns('',
     url(r'^', include('agiliqpages.urls')),
-	url(r'^forum/', include('dinette.urls')),
+	url(r'^demo/dinette/', include('dinette.urls')),
 	url(r'^blog/', include('blogango.urls')),
     url(r'^pystories/', include('pystories.urls')),
     url(r'^login/$', login, name='dinette_login'),

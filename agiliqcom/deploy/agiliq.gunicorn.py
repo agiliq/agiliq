818bd59c4857c3d7c2da5a940d4d8b3c9b3c1673
Javed Khan <javed@agiliq.com>
2013-08-20 10:53:26 +0530

    decrease num workers

--- agiliqcom/deploy/agiliq.gunicorn.py
+++ agiliqcom/deploy/agiliq.gunicorn.py
@@ -1,7 +1,7 @@
 import os
 
 bind = "127.0.0.1:8000"
-workers = (os.sysconf("SC_NPROCESSORS_ONLN") * 2) + 1
+workers = os.sysconf("SC_NPROCESSORS_ONLN") + 1
 loglevel = "debug"
 proc_name = "agiliq"
 daemon = False
--- agiliqcom/deploy/parsley.gunicorn.py
+++ agiliqcom/deploy/parsley.gunicorn.py
@@ -1,7 +1,7 @@
 import os
 
 bind = "127.0.0.1:8003"
-workers = (os.sysconf("SC_NPROCESSORS_ONLN") * 2) + 1
-loglevel = "error"
+workers = os.sysconf("SC_NPROCESSORS_ONLN") + 1
+loglevel = "debug"
 proc_name = "agiliq"
 daemon = False

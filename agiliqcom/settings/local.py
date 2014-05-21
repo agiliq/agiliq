ef70747a9bde248b8d82d3981258e8bc3b7039b5
manjunath24 <hugar.manjunathrt@gmail.com>
2014-05-21 13:19:17 +0530

    first commit for heroku

---
+++ agiliqcom/settings/local.py
@@ -0,0 +1,29 @@
+from .common import *
+
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'agiliq',                      # Or path to database file if using sqlite3.
+        'USER': 'agiliq',                      # Not used with sqlite3.
+        'PASSWORD': 'agiliq',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+BACKTYPE_API_KEY = ''
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY") or '6567duysgdu678w6e86wed'
+
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')

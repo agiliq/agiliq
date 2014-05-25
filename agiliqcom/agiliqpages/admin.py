a81daceac2902751783ef4846ab629a3d3940929
Shabda Raaj <shabda@agiliq.com>
2014-05-25 08:53:09 +0530

    Pep8 fixes (via autopep8)

--- agiliqcom/agiliqpages/admin.py
+++ agiliqcom/agiliqpages/admin.py
@@ -9,6 +9,7 @@ class ContentBlockAdmin(admin.ModelAdmin):
 
 class ContactUsListAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone', 'email', 'company', 'created_at')
+
     class Meta:
         model = ContactUs
 
@@ -17,21 +18,23 @@ class TestimonialInline(admin.TabularInline):
     model = Testimonial
     extra = 1
 
+
 class ContactAdmin(admin.ModelAdmin):
     model = Contact
     list_display = ["name", "has_testimonial", "is_active"]
-    inlines = [ TestimonialInline ]
+    inlines = [TestimonialInline]
 
 
 class ContactInline(admin.TabularInline):
     model = Contact
     extra = 1
 
+
 class ClientAdmin(admin.ModelAdmin):
     list_display = ["name", "url", "is_active"]
     list_filter = ['is_active']
     change_form_template = "admin/client_change_form.html"
-    inlines = [ ContactInline ]
+    inlines = [ContactInline]
 
 
 class TeamMemberAdmin(admin.ModelAdmin):
@@ -41,9 +44,10 @@ class TeamMemberAdmin(admin.ModelAdmin):
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "user":
             kwargs["queryset"] = User.objects.filter(is_staff=True,
-                                is_active=True)
+                                                     is_active=True)
         return super(TeamMemberAdmin, self).formfield_for_foreignkey(db_field,
-            request, **kwargs)
+                                                                     request, **kwargs)
+
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ['name', 'is_active']
--- agiliqcom/agiliqpages/urls.py
+++ agiliqcom/agiliqpages/urls.py
@@ -3,20 +3,25 @@ from django.conf import settings
 
 from agiliqpages.models import TeamMember
 from agiliqpages.views import (CachedListView, CachedTemplateView, error_page,
-    UserCreationView)
+                               UserCreationView)
 
 urlpatterns = patterns('',
-    url('^$', CachedTemplateView.as_view(template_name='agiliqpages/index.html', extra_context={'sitepage': 'home'}), name='agiliqpages_index'),
-    url('^registration/$', UserCreationView.as_view(), name='registration'),
-    url('^whatwedo$', CachedTemplateView.as_view(template_name='agiliqpages/whatwedo.html', extra_context={'sitepage': 'whatwedo'}), name='agiliqpages_whatwedo'),
-    url('^whoweare$', CachedListView.as_view(template_name='agiliqpages/whoweare.html', queryset=TeamMember.objects.exclude(active=False).order_by('ordering'), extra_context={
-            'sitepage': 'whoweare'
-        }), name='agiliqpages_whoweare'),
-    url('^thankyou$', CachedTemplateView.as_view(template_name='agiliqpages/thankyou.html', extra_context={}), name='agiliqpages_thankyou'),
-    url('^errorpage-500$', error_page, name='agiliqpages_error_page'),
-)
+                       url('^$', CachedTemplateView.as_view(
+                           template_name='agiliqpages/index.html', extra_context={'sitepage': 'home'}), name='agiliqpages_index'),
+                       url('^registration/$', UserCreationView.as_view(), name='registration'),
+                       url('^whatwedo$', CachedTemplateView.as_view(
+                           template_name='agiliqpages/whatwedo.html', extra_context={'sitepage': 'whatwedo'}), name='agiliqpages_whatwedo'),
+                       url('^whoweare$', CachedListView.as_view(template_name='agiliqpages/whoweare.html', queryset=TeamMember.objects.exclude(active=False).order_by('ordering'), extra_context={
+                           'sitepage': 'whoweare'
+                       }), name='agiliqpages_whoweare'),
+                       url('^thankyou$', CachedTemplateView.as_view(
+                           template_name='agiliqpages/thankyou.html', extra_context={}), name='agiliqpages_thankyou'),
+                       url('^errorpage-500$', error_page, name='agiliqpages_error_page'),
+                       )
 
 urlpatterns += patterns('agiliqpages.views',
-    url('^contactus$', 'contact_us', {"template": "agiliqpages/contactus.html"}, name='agiliqpages_contactus'),
-    url('^ourwork$', 'our_work', {'template': 'agiliqpages/ourwork.html'}, name='agiliqpages_ourwork'),
-)
+                        url('^contactus$', 'contact_us', {
+                            "template": "agiliqpages/contactus.html"}, name='agiliqpages_contactus'),
+                        url('^ourwork$', 'our_work', {
+                            'template': 'agiliqpages/ourwork.html'}, name='agiliqpages_ourwork'),
+                        )

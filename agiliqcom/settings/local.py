25b8df71bf480618e807808e9b29c7bd4eff6edb
Shabda Raaj <shabda@agiliq.com>
2014-05-25 19:49:02 +0530

    Enable the herokuapp domain

--- agiliqcom/settings/local.py
+++ agiliqcom/settings/local.py
@@ -42,3 +42,4 @@ if 'agiliq_heroku' in os.environ:
 
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    ALLOWED_HOSTS += [".herokuapp.com"]

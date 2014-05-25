592e8ea54fb2c539fdad2a7f6c9d5ac0ddb41e47
Shabda Raaj <shabda@agiliq.com>
2014-05-25 19:02:31 +0530

    Disable debug

--- agiliqcom/settings/local.py
+++ agiliqcom/settings/local.py
@@ -37,5 +37,7 @@ if 'agiliq_heroku' in os.environ:
     import dj_database_url
     DATABASES['default'] =  dj_database_url.config()
 
+    DEBUG = False
+
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')

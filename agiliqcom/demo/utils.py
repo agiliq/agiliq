690cec6ce0269bbda6767106d4bb4a3ec4655d51
manjunath24 <hugar.manjunathrt@gmail.com>
2014-05-29 15:24:30 +0530

    changed port to integer

--- agiliqcom/demo/utils.py
+++ agiliqcom/demo/utils.py
@@ -22,7 +22,7 @@ def get_db(db_name):
 
 def get_mongo_cursor(db_name, collection_name, max_docs=100):
     db = pymongo.Connection(host=DB_HOST,
-                            port=DB_PORT)[db_name]
+                            port=int(DB_PORT))[db_name]
     collection = db[collection_name]
     cursor = collection.find()
     if cursor.count >= max_docs:

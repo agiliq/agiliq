e09e4c70900e42cc8ef54857cb37eb284f9dc775
manjunath24 <hugar.manjunathrt@gmail.com>
2014-06-02 17:25:39 +0530

    convert merchant_id to integer

--- agiliqcom/settings/common.py
+++ agiliqcom/settings/common.py
@@ -228,116 +228,118 @@ LOGIN_URL  = "/login"
 LOGIN_REDIRECT_URL = '/'
 
 # Merchant Demo
-
+MERCHANT_ID = ""
+if 'beanstream_MERCHANT_ID' in os.environ:
+    MERCHANT_ID = int(os.environ.get('beanstream_MERCHANT_ID'))
 MERCHANT_TEST_MODE = True
 MERCHANT_SETTINGS = {
     "authorize_net": {
-        "LOGIN_ID" : os.environ.get('authorize_net_LOGIN_ID') ,
-        "TRANSACTION_KEY" : os.environ.get('authorize_net_TRANSACTION_KEY') ,
-        "MD5_HASH": os.environ.get('authorize_net_MD5_HASH')
+        "LOGIN_ID" : os.environ.get('authorize_net_LOGIN_ID') or "",
+        "TRANSACTION_KEY" : os.environ.get('authorize_net_TRANSACTION_KEY') or "" ,
+        "MD5_HASH": os.environ.get('authorize_net_MD5_HASH') or ""
     },
 
     "pay_pal": {
-        "WPP_USER" : os.environ.get('pay_pal_WPP_USER') ,
-        "WPP_PASSWORD" : os.environ.get('pay_pal_WPP_PASSWORD') ,
-        "WPP_SIGNATURE" : os.environ.get('pay_pal_WPP_SIGNATURE') ,
-        "RECEIVER_EMAIL" : os.environ.get('pay_pal_RECEIVER_EMAIL'),
+        "WPP_USER" : os.environ.get('pay_pal_WPP_USER') or "" ,
+        "WPP_PASSWORD" : os.environ.get('pay_pal_WPP_PASSWORD') or "" ,
+        "WPP_SIGNATURE" : os.environ.get('pay_pal_WPP_SIGNATURE') or "" ,
+        "RECEIVER_EMAIL" : os.environ.get('pay_pal_RECEIVER_EMAIL') or "",
         },
 
     "eway": {
-        "CUSTOMER_ID" : os.environ.get('eway_CUSTOMER_ID'),
-        "USERNAME" : os.environ.get('eway_USERNAME') ,
-        "PASSWORD" : os.environ.get('eway_PASSWORD')
+        "CUSTOMER_ID" : os.environ.get('eway_CUSTOMER_ID') or "",
+        "USERNAME" : os.environ.get('eway_USERNAME') or "" ,
+        "PASSWORD" : os.environ.get('eway_PASSWORD') or ""
         },
 
     "google_checkout": {
         # GOOGLE CHECKOUT SETTINGS
-        "MERCHANT_ID" : os.environ.get('google_checkout_MERCHANT_ID') ,
-        "MERCHANT_KEY" : os.environ.get('google_checkout_MERCHANT_KEY')
+        "MERCHANT_ID" : os.environ.get('google_checkout_MERCHANT_ID') or "" ,
+        "MERCHANT_KEY" : os.environ.get('google_checkout_MERCHANT_KEY') or ""
         },
 
     "world_pay": {
         # WORLDPAY settings
-        "HOSTED_URL_TEST": os.environ.get('world_pay_HOSTED_URL_TEST') ,
-        "HOSTED_URL_LIVE" : os.environ.get('world_pay_HOSTED_URL_LIVE') ,
+        "HOSTED_URL_TEST": os.environ.get('world_pay_HOSTED_URL_TEST') or "https://select-test.wp3.rbsworldpay.com/wcc/purchase",
+        "HOSTED_URL_LIVE" : os.environ.get('world_pay_HOSTED_URL_LIVE') or "https://secure.wp3.rbsworldpay.com/wcc/purchase",
         "INSTALLATION_ID_TEST": os.environ.get('world_pay_INSTALLATION_ID_TEST') ,
-        "MD5_SECRET_KEY": os.environ.get('world_pay_MD5_SECRET_KEY')
+        "MD5_SECRET_KEY": os.environ.get('world_pay_MD5_SECRET_KEY') or ""
         },
 
     "stripe": {
         # Stripe settings
-        "API_KEY" : os.environ.get('stripe_API_KEY'),
-        "PUBLISHABLE_KEY" : os.environ.get('stripe_PUBLISHABLE_KEY')
+        "API_KEY" : os.environ.get('stripe_API_KEY') or "",
+        "PUBLISHABLE_KEY" : os.environ.get('stripe_PUBLISHABLE_KEY') or ""
         },
 
     # Paylane Settings
     "paylane": {
-        "USERNAME": os.environ.get('paylane_USERNAME') ,
-        "PASSWORD": os.environ.get('paylane_PASSWORD')
+        "USERNAME": os.environ.get('paylane_USERNAME') or "" ,
+        "PASSWORD": os.environ.get('paylane_PASSWORD') or ""
         },
 
     "samurai": {
         # Samurai Merchant Key
-        "MERCHANT_KEY" : os.environ.get('samurai_MERCHANT_KEY') ,
-        "MERCHANT_PASSWORD" : os.environ.get('samurai_MERCHANT_PASSWORD'),
-        "PROCESSOR_TOKEN" : os.environ.get('samurai_PROCESSOR_TOKEN')
+        "MERCHANT_KEY" : os.environ.get('samurai_MERCHANT_KEY') or "" ,
+        "MERCHANT_PASSWORD" : os.environ.get('samurai_MERCHANT_PASSWORD') or "",
+        "PROCESSOR_TOKEN" : os.environ.get('samurai_PROCESSOR_TOKEN') or ""
         },
 
     "amazon_fps": {
-        "AWS_ACCESS_KEY" : os.environ.get('amazon_fps_AWS_ACCESS_KEY') ,
-        "AWS_SECRET_ACCESS_KEY" : os.environ.get('amazon_fps_AWS_SECRET_ACCESS_KEY')
+        "AWS_ACCESS_KEY" : os.environ.get('amazon_fps_AWS_ACCESS_KEY') or "" ,
+        "AWS_SECRET_ACCESS_KEY" : os.environ.get('amazon_fps_AWS_SECRET_ACCESS_KEY') or ""
         },
 
     "braintree_payments": {
-        "MERCHANT_ACCOUNT_ID" : os.environ.get('braintree_payments_MERCHANT_ACCOUNT_ID'),
-        "PUBLIC_KEY" : os.environ.get('braintree_payments_PUBLIC_KEY') ,
-        "PRIVATE_KEY" : os.environ.get('braintree_payments_PRIVATE_KEY')
+        "MERCHANT_ACCOUNT_ID" : os.environ.get('braintree_payments_MERCHANT_ACCOUNT_ID') or "",
+        "PUBLIC_KEY" : os.environ.get('braintree_payments_PUBLIC_KEY') or "" ,
+        "PRIVATE_KEY" : os.environ.get('braintree_payments_PRIVATE_KEY') or ""
         },
 
     # WePay Settings
     "we_pay": {
-        "CLIENT_ID": os.environ.get('we_pay_CLIENT_ID') ,
-        "CLIENT_SECRET": os.environ.get('we_pay_CLIENT_SECRET'),
-        "ACCOUNT_ID": os.environ.get('we_pay_ACCOUNT_ID') ,
-        "ACCESS_TOKEN": os.environ.get('we_pay_ACCESS_TOKEN') ,
+        "CLIENT_ID": os.environ.get('we_pay_CLIENT_ID') or "" ,
+        "CLIENT_SECRET": os.environ.get('we_pay_CLIENT_SECRET') or "",
+        "ACCOUNT_ID": os.environ.get('we_pay_ACCOUNT_ID') or "" ,
+        "ACCESS_TOKEN": os.environ.get('we_pay_ACCESS_TOKEN') or "" ,
     },
    "beanstream": {
-        "MERCHANT_ID": int(os.environ.get('beanstream_MERCHANT_ID')) ,
-        "LOGIN_COMPANY": os.environ.get('beanstream_LOGIN_COMPANY') ,
-        "LOGIN_USER": os.environ.get('beanstream_LOGIN_USER') ,
-        "LOGIN_PASSWORD": os.environ.get('beanstream_LOGIN_PASSWORD') ,
-        "HASH_ALGORITHM"   : os.environ.get('beanstream_HASH_ALGORITHM'),
-        "HASHCODE"  : os.environ.get('beanstream_HASHCODE'),
+        "MERCHANT_ID": MERCHANT_ID,
+        "LOGIN_COMPANY": os.environ.get('beanstream_LOGIN_COMPANY') or "" ,
+        "LOGIN_USER": os.environ.get('beanstream_LOGIN_USER') or "" ,
+        "LOGIN_PASSWORD": os.environ.get('beanstream_LOGIN_PASSWORD') or ""  ,
+        "HASH_ALGORITHM"   : os.environ.get('beanstream_HASH_ALGORITHM') or "" ,
+        "HASHCODE"  : os.environ.get('beanstream_HASHCODE') or "" ,
         "PAYMENT_PROFILE_PASSCODE":"",
     },
 
     "chargebee": {
-        "API_KEY": os.environ.get('chargebee_API_KEY'),
-        "SITE": os.environ.get('chargebee_SITE')
+        "API_KEY": os.environ.get('chargebee_API_KEY') or "" ,
+        "SITE": os.environ.get('chargebee_SITE') or ""
     },
 
     "bitcoin": {
-        "RPCUSER": os.environ.get('bitcoin_RPCUSER'),
-        "RPCPASSWORD": os.environ.get('bitcoin_RPCPASSWORD') ,
-        "HOST": os.environ.get('bitcoin_HOST') ,
-        "PORT": os.environ.get('bitcoin_PORT') ,
-        "ACCOUNT": os.environ.get('bitcoin_ACCOUNT'),
+        "RPCUSER": os.environ.get('bitcoin_RPCUSER') or "" ,
+        "RPCPASSWORD": os.environ.get('bitcoin_RPCPASSWORD') or ""  ,
+        "HOST": os.environ.get('bitcoin_HOST') or ""  ,
+        "PORT": os.environ.get('bitcoin_PORT') or ""  ,
+        "ACCOUNT": os.environ.get('bitcoin_ACCOUNT') or "" ,
     },
 
     "ogone_payments": {
-        "SHA_PRE_SECRET": os.environ.get('ogone_payments_SHA_PRE_SECRET') ,
-        "SHA_POST_SECRET": os.environ.get('ogone_payments_SHA_POST_SECRET'),
-        "HASH_METHOD": os.environ.get('ogone_payments_HASH_METHOD') ,
+        "SHA_PRE_SECRET": os.environ.get('ogone_payments_SHA_PRE_SECRET') or ""  ,
+        "SHA_POST_SECRET": os.environ.get('ogone_payments_SHA_POST_SECRET') or "" ,
+        "HASH_METHOD": os.environ.get('ogone_payments_HASH_METHOD') or ""  ,
         "PRODUCTION": not MERCHANT_TEST_MODE,
-        "PSPID": os.environ.get('ogone_payments_PSPID'),
-        "OGONE_TEST_URL": os.environ.get('ogone_payments_OGONE_TEST_URL') ,
-        "OGONE_PROD_URL": os.environ.get('ogone_payments_OGONE_PROD_URL')
+        "PSPID": os.environ.get('ogone_payments_PSPID') or "" ,
+        "OGONE_TEST_URL": os.environ.get('ogone_payments_OGONE_TEST_URL') or 'https://secure.ogone.com/ncol/test/orderstandard.asp',
+        "OGONE_PROD_URL": os.environ.get('ogone_payments_OGONE_PROD_URL') or 'https://secure.ogone.com/ncol/prod/orderstandard.asp'
     },
 
     "pin": {
-        "SECRET": os.environ.get('pin_SECRET')
+        "SECRET": os.environ.get('pin_SECRET') or ""
     }
-    }
+}
 
 # PAYPAL SETTINGS
 PAYPAL_TEST = True

08d08f0cad0c895d0408363666a2a153cf9def4b
manjunath24 <hugar.manjunathrt@gmail.com>
2014-06-04 15:59:41 +0530

    email configuration

--- agiliqcom/settings/heroku.py
+++ agiliqcom/settings/heroku.py
@@ -29,3 +29,7 @@ COMPRESS_ENABLED = "DJANGO_COMPRESS" in os.environ
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS += [".herokuapp.com"]
+
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_HOST_USER = "postmaster@agiliq.com"
+EMAIL_HOST_PASSWORD = "43ze4je2vu21"

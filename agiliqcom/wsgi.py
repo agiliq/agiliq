2ded9aab5c886be3539aa65d03684402fe874624
Shabda Raaj <shabda@agiliq.com>
2014-06-07 23:33:53 +0530

    Layout requirements file better

--- agiliqcom/wsgi.py
+++ agiliqcom/wsgi.py
@@ -4,8 +4,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "agiliqcom.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
